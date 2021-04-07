7385bb33397f729480c9046c4368efe851aafebd
tmdgh <tmdghks3264@naver.com>
2021-04-07 21:52:53 +0900

    Change the RecyclerView UI

--- app/src/main/java/com/router/investmentcalendar/ui/list/InvestListRecyclerViewAdapter.kt
+++ app/src/main/java/com/router/investmentcalendar/ui/list/InvestListRecyclerViewAdapter.kt
@@ -2,10 +2,12 @@ package com.router.investmentcalendar.ui.list
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
@@ -31,6 +33,7 @@ class InvestListRecyclerViewAdapter(private val dataSet: ArrayList<InvestItem>) 
         val profit_percent_tv : TextView
         val item_date_tv : TextView
         val item_linearlayout : ConstraintLayout
+        val profit_iv : ImageView
 
 
         init {
@@ -40,6 +43,7 @@ class InvestListRecyclerViewAdapter(private val dataSet: ArrayList<InvestItem>) 
             profit_percent_tv = view.findViewById(R.id.item_profitpercent_tv)
             item_date_tv = view.findViewById(R.id.item_date_tv)
             item_linearlayout = view.findViewById(R.id.item_linearlayout)
+            profit_iv = view.findViewById(R.id.profit_iv)
 
         }
 
@@ -60,17 +64,19 @@ class InvestListRecyclerViewAdapter(private val dataSet: ArrayList<InvestItem>) 
         viewHolder.start_asset_tv.text = decimalFormat.format(dataSet[position].start_asset).toString()+"원"
         viewHolder.finish_asset_tv.text = decimalFormat.format(dataSet[position].finish_asset).toString()+"원"
         if(dataSet[position].profit_percent>=0){
-            viewHolder.item_linearlayout.setBackgroundColor(Color.parseColor("#D4E9CE"))
+            viewHolder.item_linearlayout.setBackgroundColor(Color.parseColor("#388E3C"))
             viewHolder.profit_percent_tv.setTextColor(Color.parseColor("#4CAF50"))
             viewHolder.profit_asset_tv.setTextColor(Color.parseColor("#4CAF50"))
             viewHolder.profit_asset_tv.text = "+"+decimalFormat.format(dataSet[position].profit_asset).toString()+"원"
             viewHolder.profit_percent_tv.text = "+"+dataSet[position].profit_percent.toString()+"%"
+            viewHolder.profit_iv.setBackgroundResource(R.drawable.ic_baseline_arrow_upward_24)
         }else{
-            viewHolder.item_linearlayout.setBackgroundColor(Color.parseColor("#E8CFCF"))
+            viewHolder.item_linearlayout.setBackgroundColor(Color.parseColor("#E91E63"))
             viewHolder.profit_percent_tv.setTextColor(Color.parseColor("#E91E63"))
             viewHolder.profit_asset_tv.setTextColor(Color.parseColor("#E91E63"))
             viewHolder.profit_asset_tv.text = decimalFormat.format(dataSet[position].profit_asset).toString()+"원"
             viewHolder.profit_percent_tv.text = dataSet[position].profit_percent.toString()+"%"
+            viewHolder.profit_iv.setBackgroundResource(R.drawable.ic_baseline_arrow_downward_24)
         }
 
         viewHolder.item_date_tv.text = dataSet[position].date
